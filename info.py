ab41fcfbf8bcd3b57a25767f48e1bb21a57589f1
RancyChepchirchir <kosgeyrancy2@gmail.com>
2021-04-04 07:26:50 +0300

    Inital Commits

---
+++ info.py
@@ -0,0 +1,36 @@
+projects = [
+    ('A speech recognition based digital assistant written in Python like Cortana', 'Bruno'),
+    ('Automation Scripts with selenium and other tools written in Python', 'Automation-Bots'),
+    ('Various scrapping scripts in Python and Cheerio JS', 'Scrapping'),
+    ('Python implementation of classic arcade game Space Invasion', 'Spaceship'),
+    ('A desktop application to delete thousands of files instantaneously using Tkinter', 'AutoDel'),
+    ('Pomodoro clock as desktop notifications written in Python', 'PyPomodoro'),
+    ('An easy to use web implementation of Thesaurus using Flask', 'Vocab-Bird'),
+    ('Google\'s Stocks price prediction using Recurrent Neural Networks', 'Stock-Price-Prediction-with-RNN'), 
+    ('A social media like twitter in it\'s earlier days using Flask', 'The-Social-Network'), 
+    ('An image search engine using OpenCv based on Chi-Square distance metric', 'Image-Search-Engine')
+]
+
+
+skills = [
+    'Full Stack Python Development',
+    'Machine Learning',
+    'Data mining & Scrapping',
+    'Chatbot Development',
+    'REST APIs design & implementation',
+    
+]
+
+
+tools = [
+    ('tensorflow.png', 'Tensorflow'),
+    ('mongo.png', 'MongoDB'),
+    ('opencv.png', 'OpenCv'),
+    ('flask.png', 'Flask'),
+    ('postgres.png', 'PostgreSQL'),
+    ('javascript.png', 'Javascript'),
+    ('python.png', 'Python'),
+    ('django.png', 'Django'),
+    ('scikit.png', 'Scikit-Learn'),
+    ('bootstrap.png', 'Bootstrap')
+]
